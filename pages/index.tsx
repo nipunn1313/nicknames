b831f230a3c172353d240246f8260fb9baa03fb1
Nipunn Koorapati <nipunn1313@gmail.com>
2022-04-30 14:10:30 -0700

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <div>hello</div>
+      <div>world</div>
+    </div>
+  )
+}
+
+export default Home
